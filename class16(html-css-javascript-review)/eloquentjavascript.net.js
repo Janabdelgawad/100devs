24a1dd4ed1abbf74c89cecdc67fd0edc1e30b1b0
Jana <janaakamaal2004@gmail.com>
2025-08-28 00:33:22 +0300

    Update eloquentjavascript.net.js

--- class16(html-css-javascript-review)/eloquentjavascript.net.js
+++ class16(html-css-javascript-review)/eloquentjavascript.net.js
@@ -5,3 +5,20 @@ function ascendingHash7(){
       console.log(hash);
   }
 }
+
+function fizzBuzz100(){
+    for(let i = 1; i <=100; i++) {
+        if(i % 3 == 0 && i % 5 == 0) {
+            console.log('FizzBuzz');
+        }
+        else if(i % 3 == 0){
+            console.log('Fizz');
+        }
+        else if (i % 5 == 0) {
+            console.log('Buzz');
+        }
+        else {
+          console.log(i);  
+        }
+    }  
+}
